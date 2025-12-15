fd2575081f08069bb8427074baecce4d357e8267
Dmitry Fomenko <fomedima@gmail.com>
2025-12-15 12:30:44 +0300

    Add graphql config

--- apps-cms/config/plugins.ts
+++ apps-cms/config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 12,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: true,
+        introspection: true,
+      },
+    },
+  },
+});
